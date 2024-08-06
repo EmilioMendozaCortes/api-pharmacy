987505badef0762091e21d44a58c5e085370c89a
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-08-06 14:44:53 -0600

    Creación del servidor

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Importación de dependencias
+import express from 'express';
+
+// Creación del servidor
+const app = express();
+export default app;
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Importación de dependencias
+import app from "./app";
+
+// Ejecución del servidor
+app.listen(3000,  ()=> {
+    console.log("Server is running on port 3000");
+});
